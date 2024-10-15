9efca7c296bc38c6913897a3c6e6aa62fbed6d74
Hoher2000 <160623007+Hoher2000@users.noreply.github.com>
2024-10-15 15:02:33 +0300

    Update hoherlang_objects.h

--- garbage_cleaner/hoherlang_objects.h
+++ garbage_cleaner/hoherlang_objects.h
@@ -1,5 +1,7 @@
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
+
+#include "low_level_stack.h"
 
 typedef struct SnekObject snek_object_t;
 
@@ -9,7 +11,9 @@ typedef struct {
 } snek_array_t;
 
 typedef struct {
-  snek_object_t *x, *y, *z;
+  snek_object_t *x;
+  snek_object_t *y;
+  snek_object_t *z;
 } snek_vector_t;
 
 typedef enum SnekObjectKind {
@@ -17,13 +21,13 @@ typedef enum SnekObjectKind {
   FLOAT,
   STRING,
   VECTOR3,
-  ARRAY
+  ARRAY,
 } snek_object_kind_t;
 
 typedef union SnekObjectData {
   int v_int;
   float v_float;
-  char* v_string;
+  char *v_string;
   snek_vector_t v_vector3;
   snek_array_t v_array;
 } snek_object_data_t;
@@ -31,17 +35,4 @@ typedef union SnekObjectData {
 typedef struct SnekObject {
   snek_object_kind_t kind;
   snek_object_data_t data;
-  int refcount;
 } snek_object_t;
-
-snek_object_t *new_snek_integer(int);
-snek_object_t *new_snek_float(float);
-snek_object_t *new_snek_string(char *);
-snek_object_t *new_snek_vector3(snek_object_t *, snek_object_t *, snek_object_t *);
-snek_object_t *new_snek_array(size_t);
-bool snek_array_set(snek_object_t *, size_t, snek_object_t *);
-snek_object_t *snek_array_get(snek_object_t *, size_t);
-snek_object_t *_new_snek_object();
-void refcount_inc(snek_object_t *);
-void refcount_free(snek_object_t *);
-void refcount_dec(snek_object_t *);
