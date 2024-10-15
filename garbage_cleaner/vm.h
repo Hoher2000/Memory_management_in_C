a790c39fbdb36eea7388afd9aff17f843d4e2b7b
Hoher2000 <160623007+Hoher2000@users.noreply.github.com>
2024-10-15 16:50:19 +0300

    Update vm.h

--- garbage_cleaner/vm.h
+++ garbage_cleaner/vm.h
@@ -1,3 +1,5 @@
+#pragma once
+#include "hoherlang_objects.h"
 #include "low_level_stack.h"
 
 typedef struct VirtualMachine {
@@ -5,5 +7,15 @@ typedef struct VirtualMachine {
   stack_t *objects;
 } vm_t;
 
+typedef struct StackFrame {
+  stack_t *references;
+} frame_t;
+
 vm_t *vm_new();
 void vm_free(vm_t *vm);
+
+
+void vm_frame_push(vm_t *vm, frame_t *frame);
+frame_t *vm_new_frame(vm_t *vm);
+
+void frame_free(frame_t *frame);
